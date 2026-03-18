6d33a4ac8206bfcb028e6e65482f57691161f13c
Diogo Guerra <d.g.carramona@gmail.com>
2026-03-18 13:31:29 +0000

    chore(tests): add smoke test environment setup for Coprogo
    - Introduced `application-smoke.properties` for environment-specific configuration.
    - Updated `CoprogoTest` to include the "smoke" environment.

--- coprogo/src/test/kotlin/tech/justdev/CoprogoTest.kt
+++ coprogo/src/test/kotlin/tech/justdev/CoprogoTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import jakarta.inject.Inject
 
-@MicronautTest(transactional = false)
+@MicronautTest(transactional = false, environments = ["smoke"])
 class CoprogoTest {
 
     @Inject
