3879ecf3eb96633a897a69a17262165ad82d6500
Diogo Guerra <d.g.carramona@gmail.com>
2026-07-26 14:27:41 +0100

    regenerated http client

--- frontend/src/app/infrastructure/api/generated/api/api.ts
+++ frontend/src/app/infrastructure/api/generated/api/api.ts
@@ -2,6 +2,6 @@ export * from './groups.service';
 import { GroupsService } from './groups.service';
 export * from './ledger.service';
 import { LedgerService } from './ledger.service';
-export * from './revenueDistribution.service';
-import { RevenueDistributionService } from './revenueDistribution.service';
+export * from './revenue-distribution.service';
+import { RevenueDistributionService } from './revenue-distribution.service';
 export const APIS = [GroupsService, LedgerService, RevenueDistributionService];
